d832e2c3f393cf68190eadffd0bc9c5d886a2c50
Rebekah Richardson <bekahrichardson0422@gmail.com>
2017-01-24 19:54:39 -0700

    First five callback problems

---
+++ callbackPractice.js
@@ -0,0 +1,176 @@
+/* In this repo your job is to write functions to make each function call work properly.
+Below is a sample problem
+
+  //code here for sayHi
+
+   sayHi('Hi Katie', function(thingToSay){
+      alert(thingToSay);
+   });
+
+
+and what you should write is the sayHi function that makes the code above work,
+
+
+   var sayHi = function(str, cb){
+    cb(str);
+   }
+
+   sayHi('Hi Katie', function(thingToSay){
+      alert(thingToSay); //should alert ('Hi Katie')'
+   });
+
+
+*/
+
+
+
+
+  function first(arr, cb) {
+    var firstName = arr[0];
+    cb(firstName);
+  }
+
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+first(names, function(firstName){
+  console.log('The first name in names is ' + firstName)
+});
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+  function last(arr, cb) {
+    var lastName = arr[arr.length -1];
+    cb(lastName);
+  }
+
+last(names, function(lastName){
+  console.log('The last name in names is ' + lastName);
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+
+function multiply(num1, num2, cb) {
+  var answer = (num1 * num2);
+  cb(answer);
+}
+
+multiply(4, 3, function(answer){
+  console.log('The answer is ' + answer); //should console.log 12
+})
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+var contains = function(name, string, cb) {
+  var nameInString;
+  if (name.indexOf(string) === -1) {
+    nameInString = false;
+  } else {
+    nameInString = true;
+  }
+  cb(nameInString);
+}
+
+
+contains(names, 'Colt', function(result){
+  if(result === true){
+    console.log('Colt is in the array');
+  } else {
+    console.log('Colt is not in the array');
+  }
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+var uniq = function(array, cb) {
+  var uniqueArray = [];
+
+  array.forEach(function(item) {
+  if(uniqueArray.indexOf(item) === -1) {
+    uniqueArray.push(item);
+  }
+});
+cb(uniqueArray);
+};
+
+
+uniq(names, function(uniqArr){
+  console.log('The new names array with all the duplicate items removed is ', uniqArr);
+});
+
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+    
+
+each(names, function(item, indice){
+  console.log('The item in the ' + indice + ' position is ' + item)
+});
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+function getUserById(arr, cb) {
+  var user = arr[2];
+  cb(user);
+}
+
+ //code here for getUserById
+
+var users = [
+  {
+    id: '12d',
+    email: 'tyler@gmail.com',
+    name: 'Tyler',
+    address: '167 East 500 North'
+  },
+  {
+    id: '15a',
+    email: 'cahlan@gmail.com',
+    name: 'Cahlan',
+    address: '135 East 320 North'
+  },
+  {
+    id: '16t',
+    email: 'ryan@gmail.com',
+    name: 'Ryan',
+    address: '192 East 32 North'
+  },
+];
+
+getUserById(users, '16t', function(user){
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address);
+});
